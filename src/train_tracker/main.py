c23077427e31ded1589185df079fd24bb8fa58ae
George Kaye <georgejkaye@gmail.com>
2024-08-02 11:00:32 +0100

    [impl] Remove redundant main boilerplate

--- src/train_tracker/main.py
+++ src/train_tracker/main.py
@@ -1,18 +1,7 @@
-import sys
-
-from train_tracker.api import authenticate
 from train_tracker.data.database import connect, disconnect
 from train_tracker.record import add_to_logfile
-from train_tracker.network import setup_network
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python " + sys.argv[0] + " <output>")
-        exit(1)
-
-    output_file = sys.argv[1]
-
-    setup_network()
     (conn, cur) = connect()
     add_to_logfile(conn, cur)
     disconnect(conn, cur)
