9f692b8979bff5a6fc398cfbc64ad891b7093dab
George Kaye <georgejkaye@gmail.com>
2024-08-09 23:44:30 +0100

    [env] Add client to docker network

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ]
+  },
+  output: "standalone",
+}
 
-export default nextConfig;
+export default nextConfig
