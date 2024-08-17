a6814a25a9d2efc7954c3f17d724e96b4d5a0d13
George Kaye <georgejkaye@gmail.com>
2024-08-17 10:06:58 +0100

    [fix] Stop nav bar being hidden on new page load

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <TopBar />
-        <main className="min-h-screen items-center flex flex-col justify-between py-4 px-2 lg:px-0">
+        <main className="items-center flex flex-col justify-between py-4 px-2 lg:px-0">
           {children}
         </main>
       </body>
