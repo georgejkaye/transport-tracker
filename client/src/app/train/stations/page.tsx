a51e91fc5ba132755e750a74200697a746ac1d9a
George Kaye <georgejkaye@gmail.com>
2024-08-24 22:48:56 +0100

    [feat] Make stations page look nice

--- client/src/app/train/stations/page.tsx
+++ client/src/app/train/stations/page.tsx
@@ -1,27 +1,40 @@
 "use client"
 
 import { getTrainStations } from "@/app/data"
+import { Line } from "@/app/line"
 import { Loader } from "@/app/loader"
 import { TrainStationData } from "@/app/structs"
+import { linkStyle } from "@/app/styles"
 import Image from "next/image"
+import Link from "next/link"
 import { useEffect, useState } from "react"
+
+const StationCallStat = (props: { name: string; stat: number }) => {
+  let { name, stat } = props
+  return (
+    <div className="flex flex-row gap-2 items-center">
+      <div className="text-xs">{name}</div>
+      <div className="w-6 text-center">{stat}</div>
+    </div>
+  )
+}
 
 const StationRow = (props: { station: TrainStationData }) => {
   let { station } = props
   return (
-    <div>
-      {station.name} {station.operator.name} {station.starts} {station.finishes}{" "}
-      {station.intermediates}
-      {!station.img ? (
-        ""
-      ) : (
-        <Image
-          src={station.img}
-          alt={`Station sign for ${station.name}`}
-          width={300}
-          height={200}
-        />
-      )}
+    <div className="flex flex-col lg:flex-row gap-2 lg:items-center">
+      <div className="flex flex-row gap-2">
+        <div className="">{station.crs}</div>
+        <Link href={`/train/station/${station.crs}`}>
+          <div className={`${linkStyle}`}>{station.name}</div>
+        </Link>
+      </div>
+      <div className="text-sm lg:flex-1">{station.operator.name}</div>
+      <div className="flex flex-row gap-2">
+        <StationCallStat name="Starts" stat={station.starts} />
+        <StationCallStat name="Ends" stat={station.finishes} />
+        <StationCallStat name="Passes" stat={station.intermediates} />
+      </div>
     </div>
   )
 }
@@ -43,11 +56,15 @@ const Page = () => {
     <Loader />
   ) : (
     <div>
-      {stations.map((station, i) => (
-        <div key={i}>
-          <StationRow station={station} />
-        </div>
-      ))}
+      <h1 className="text-2xl font-bold mb-2">Railway stations</h1>
+      <div className="flex flex-col gap-2">
+        {stations.map((station, i) => (
+          <div key={i} className="flex flex-col gap-2">
+            <Line />
+            <StationRow station={station} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
