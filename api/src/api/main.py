4a0f1ec79493914a2ce80b1e8a783ffe204c5c4f
George Kaye <georgejkaye@gmail.com>
2024-12-30 23:33:28 +0000

    fix: use correct connect logic for main file

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -2,6 +2,5 @@ from api.data.database import connect
 from api.record import add_to_logfile
 
 if __name__ == "__main__":
-    (conn, cur) = connect()
-    add_to_logfile(conn, cur)
-    disconnect(conn, cur)
+    with connect() as (conn, cur):
+        add_to_logfile(conn, cur)
