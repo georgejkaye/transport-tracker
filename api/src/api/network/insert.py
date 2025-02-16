57c3ed7c3e693a5268b3bf957395b320f94a2a1a
George Kaye <georgejkaye@gmail.com>
2025-02-16 21:28:48 +0000

    fix: repoint import

--- api/src/api/network/insert.py
+++ api/src/api/network/insert.py
@@ -1,11 +1,10 @@
 import sys
+
 import osmnx as ox
 
 from api.utils.database import connect
 from api.network.network import insert_station_node_to_network
-from api.data.stations import (
-    get_station_points_from_crses,
-)
+from api.data.points import get_station_points_from_crses
 
 network = ox.load_graphml(sys.argv[1])
 
