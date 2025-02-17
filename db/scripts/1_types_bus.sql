e2194e77e3c5e3d92a1df294dac61f398edaeefe
George Kaye <georgejkaye@gmail.com>
2025-02-17 22:29:28 +0000

    feat: add db procedures for getting bus stops

--- db/scripts/1_types_bus.sql
+++ db/scripts/1_types_bus.sql
@@ -15,3 +15,22 @@ CREATE TYPE BusStopData AS (
     latitude DECIMAL,
     longitude DECIMAL
 );
+
+CREATE TYPE BusStopOutData AS (
+    bus_stop_id INT,
+    atco_code TEXT,
+    naptan_code TEXT,
+    stop_name TEXT,
+    landmark_name TEXT,
+    street_name TEXT,
+    crossing_name TEXT,
+    indicator TEXT,
+    bearing TEXT,
+    locality_name TEXT,
+    parent_locality_name TEXT,
+    grandparent_locality_name TEXT,
+    town_name TEXT,
+    suburb_name TEXT,
+    latitude DECIMAL,
+    longitude DECIMAL
+);
---
+++ db/scripts/4_select_bus.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION GetBusStops (
+    p_name TEXT
+) RETURNS SETOF BusStopOutData
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+    SELECT
+        bus_stop_id,
+        atco_code,
+        naptan_code,
+        stop_name,
+        landmark_name,
+        street_name,
+        crossing_name,
+        indicator,
+        bearing,
+        locality_name,
+        parent_locality_name,
+        grandparent_locality_name,
+        town_name,
+        suburb_name,
+        latitude,
+        longitude
+    FROM BusStop
+    WHERE stop_name LIKE '%' || p_name || '%'
+    ORDER BY stop_name ASC;
+END;
+$$;
