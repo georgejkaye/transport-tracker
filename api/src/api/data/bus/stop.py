0495b26f504cb7dcc319173e85539ea44350a393
George Kaye <georgejkaye@gmail.com>
2025-03-02 15:57:10 +0000

    feat: add python wrapper for bus stop atco lookup

--- api/src/api/data/bus/stop.py
+++ api/src/api/data/bus/stop.py
@@ -123,6 +123,18 @@ def get_bus_stops(conn: Connection, search_string: str) -> list[BusStop]:
     return [row[0] for row in rows]
 
 
+def get_bus_stops_from_atcos(
+    conn: Connection, atcos: list[str]
+) -> dict[str, BusStop]:
+    register_type(conn, "BusStopOutData", register_bus_stop)
+    rows = conn.execute("SELECT GetBusStopsFromAtcos(%s)", [atcos])
+    atco_bus_stop_dict = {}
+    for row in rows:
+        bus_stop = row[0]
+        atco_bus_stop_dict[bus_stop.atco] = bus_stop
+    return atco_bus_stop_dict
+
+
 def get_bus_stop_page_url(
     bus_stop: BusStop, search_datetime: datetime = datetime.now()
 ) -> str:
