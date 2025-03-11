725db13a2a3ef01c820994912be4dc91c376921f
George Kaye <georgejkaye@gmail.com>
2025-03-11 22:32:14 +0000

    feat: add operators to all bus populate script

--- api/src/api/data/bus/populate/all.py
+++ api/src/api/data/bus/populate/all.py
@@ -1,5 +1,6 @@
 import sys
 
+from api.data.bus.populate.operators import populate_bus_operators
 from api.data.bus.populate.service import populate_bus_services
 from api.data.bus.populate.stop import populate_bus_stops
 from api.data.bus.populate.vehicle import populate_bus_vehicles
@@ -7,5 +8,6 @@ from api.utils.database import connect
 
 with connect(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]) as (conn, _):
     populate_bus_stops(conn, sys.argv[5])
-    populate_bus_services(conn, sys.argv[6])
+    populate_bus_operators(conn, sys.argv[6])
+    populate_bus_services(conn, sys.argv[7])
     populate_bus_vehicles(conn)
