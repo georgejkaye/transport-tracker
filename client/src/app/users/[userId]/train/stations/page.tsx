2b67771a62df7e199a3024f4981c6e61abdc5244
George Kaye <georgejkaye@gmail.com>
2025-03-27 22:38:59 +0000

    fix: add user id to stations page

--- client/src/app/users/[userId]/train/stations/page.tsx
+++ client/src/app/users/[userId]/train/stations/page.tsx
@@ -5,8 +5,8 @@ import { Line } from "@/app/line"
 import { Loader } from "@/app/loader"
 import { TrainStationData } from "@/app/structs"
 import { linkStyle } from "@/app/styles"
-import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 const StationCallStat = (props: { name: string; stat: number }) => {
@@ -19,8 +19,8 @@ const StationCallStat = (props: { name: string; stat: number }) => {
   )
 }
 
-const StationRow = (props: { station: TrainStationData }) => {
-  let { station } = props
+const StationRow = (props: { userId: number; station: TrainStationData }) => {
+  let { userId, station } = props
   return (
     <div className="flex flex-col lg:flex-row gap-2 lg:items-center">
       <div className="flex flex-row gap-2">
@@ -39,13 +39,19 @@ const StationRow = (props: { station: TrainStationData }) => {
   )
 }
 
-const Page = () => {
+const Page = ({ params }: { params: { userId: string } }) => {
   let [stations, setStations] = useState<TrainStationData[] | undefined>(
     undefined
   )
+  let { userId } = params
+  let userIdNumber = parseInt(userId)
+  let router = useRouter()
   useEffect(() => {
+    if (isNaN(userIdNumber)) {
+      router.push("/")
+    }
     const getStationData = async () => {
-      let stationData = await getTrainStations()
+      let stationData = await getTrainStations(userIdNumber)
       if (stationData) {
         setStations(stationData)
       }
@@ -61,7 +67,7 @@ const Page = () => {
         {stations.map((station) => (
           <div key={station.crs} className="flex flex-col gap-2">
             <Line />
-            <StationRow station={station} />
+            <StationRow userId={userIdNumber} station={station} />
           </div>
         ))}
       </div>
