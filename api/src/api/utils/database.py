3ee02b07bb697ad511abbc793317b7e90bc90814
George Kaye <georgejkaye@gmail.com>
2025-06-22 16:30:33 +0100

    refactor: tidy up database file

--- api/src/api/utils/database.py
+++ api/src/api/utils/database.py
@@ -1,8 +1,7 @@
 import sys
 from dataclasses import dataclass
 from datetime import datetime
-from decimal import Decimal, DecimalException
-from typing import Any, Callable, Optional
+from typing import Any, Optional
 
 from dotenv import load_dotenv
 from psycopg import Connection
@@ -68,60 +67,6 @@ def connect_with_env() -> DbConnection:
     return DbConnection(DbConnectionData(None, None, None, None))
 
 
-@dataclass
-class NoEscape:
-    string: str
-
-
-def int_or_none_to_str_or_none(x: Optional[int]) -> Optional[str]:
-    if x is None:
-        return None
-    return str(x)
-
-
-def optional_to_decimal(x: Optional[Any]) -> Optional[Decimal]:
-    if x is None:
-        return None
-    try:
-        dec = Decimal(str(x))
-        return dec
-    except DecimalException:
-        return None
-
-
-def str_or_none_to_str(x: str | None | NoEscape) -> str:
-    if x is None or x == "":
-        return "NULL"
-    match x:
-        case NoEscape(string):
-            return string
-        case _:
-            pass
-    replaced = x.replace("\u2019", "'")
-    return f"$${replaced}$$"
-
-
-def datetime_or_none_to_str(x: datetime | None) -> Optional[str]:
-    if x is None:
-        return None
-    else:
-        return x.isoformat()
-
-
-def number_or_none_to_str(x: int | float | Decimal | None) -> Optional[str]:
-    if x is None:
-        return None
-    else:
-        return str(x)
-
-
-def datetime_or_none_to_raw_str(x: datetime | None) -> str:
-    if x is None:
-        return "NULL"
-    else:
-        return f"'{x.isoformat()}'"
-
-
 def str_or_null_to_datetime(
     x: str | None, tz: Optional[Any] = None
 ) -> datetime | None:
@@ -136,43 +81,7 @@ def str_or_null_to_datetime(
         return None
 
 
-def list_of_str_and_none_to_postgres_str(
-    values: list[str | None | NoEscape],
-) -> list[str]:
-    return [str_or_none_to_str(value) for value in values]
-
-
-def insert(
-    conn: Connection,
-    table: str,
-    fields: list[str],
-    values: list[list[str | None | NoEscape]],
-    additional_query: str = "",
-) -> None:
-    partition_length = 500
-    partitions = [
-        values[i * partition_length : (i + 1) * partition_length]
-        for i in range((len(values) + partition_length - 1) // partition_length)
-    ]
-    rows = ",".join(fields)
-    for partition in partitions:
-        value_strings = list(
-            map(
-                lambda x: f"({','.join(list_of_str_and_none_to_postgres_str(x))})",
-                partition,
-            )
-        )
-        statement: str = f"""
-            INSERT into {table}({rows})
-            VALUES {",".join(value_strings)}
-            {additional_query}
-        """
-        conn.execute(statement.encode())
-
-
-def register_type(
-    conn: Connection, name: str, factory: Optional[Callable[..., Any]] = None
-) -> None:
+def register_type[T](conn: Connection, name: str, factory: type) -> None:
     info = CompositeInfo.fetch(conn, name)
     if info is not None:
         register_composite(info, conn, factory)
