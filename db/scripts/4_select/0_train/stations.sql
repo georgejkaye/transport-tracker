c6aa553af2fd54412b44144e76dff82442e9ed21
George Kaye <georgejkaye@gmail.com>
2025-07-18 22:22:08 +0100

    fix: make select stations by name look up on alternate names

--- db/scripts/4_select/0_train/stations.sql
+++ db/scripts/4_select/0_train/stations.sql
@@ -23,13 +23,16 @@ LANGUAGE sql
 AS
 $$
 SELECT
-    train_station_id,
-    station_crs,
-    station_name,
-    train_operator_id,
-    train_brand_id
+    train_station.train_station_id,
+    train_station.station_crs,
+    train_station.station_name,
+    train_station.train_operator_id,
+    train_station.train_brand_id
 FROM train_station
+LEFT JOIN train_station_name
+ON train_station.train_station_id = train_station_name.train_station_id
 WHERE LOWER(station_name) = LOWER(p_station_name)
+OR LOWER(alternate_station_name) = LOWER(p_station_name);
 $$;
 
 CREATE OR REPLACE FUNCTION select_station_by_name_substring (
