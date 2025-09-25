22e6387fad6e395ce5d08208d5293e91ea116d83
George Kaye <georgejkaye@gmail.com>
2025-09-25 21:56:23 +0100

    feat: update run in scripts script to only show errors

--- db/run-in-scripts.sh
+++ db/run-in-scripts.sh
@@ -6,7 +6,29 @@ DB_DIR=$( dirname $0 )
 SCRIPTS_DIR="$DB_DIR/scripts"
 SCRIPTS=$( find $SCRIPTS_DIR | sort | grep sql | grep -v schema )
 
+rm output.log
+
 for SCRIPT_FILE in $SCRIPTS;
 do
-    psql -h $DB_HOST -d $DB_NAME -U $DB_USER -a -f $SCRIPT_FILE
+    psql -h $DB_HOST -d $DB_NAME -U $DB_USER -f $SCRIPT_FILE -q 2&>> output.log
+done
+
+ERROR_OUTPUT=0
+
+cat output.log | while read line;
+do
+    if [[ $line != *"psql"* && $ERROR_OUTPUT == 0 ]];
+    then
+        continue
+    fi
+
+    if [[ $line == *"NOTICE"* ]];
+    then
+        ERROR_OUTPUT=0
+        continue
+    fi
+
+    ERROR_OUTPUT=1
+
+    echo $line
 done
