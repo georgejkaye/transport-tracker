242a7e93aa13dcec82578e6a40cd82c57ee64fd9
George Kaye <georgejkaye@gmail.com>
2025-09-28 10:59:39 +0100

    feat: begin implementing better dev db init system

---
+++ db/data/README.md
@@ -0,0 +1,3 @@
+# DB data
+
+Use this directory to store scripts for updating the db schema or adding data.
--- db/gen-data.sh
+++ db/gen-data.sh
@@ -2,12 +2,28 @@
 
 PASSWORD=$(cat /run/secrets/db_secret)
 
-DATA_FILE=/db/scripts/dump.pgdata
+DB_POPULATE_DIR=/db/.populate
 
-if [ -f $DATA_FILE ]; then
+if [ ! -d $DB_POPULATE_DIR ]
+then
+    mkdir $DB_POPULATE_DIR
+fi
+
+DATA_FILE="$DB_POPULATE_DIR/0_dump.pgdata"
+
+for EXISTING_SCRIPT in `find $DB_POPULATE_DIR -name *sql`
+do
+    if [ "$EXISTING_SCRIPT" != "$DATA_FILE" ]
+    then
+        rm $EXISTING_SCRIPT
+    fi
+done
+
+if [ -f "$DATA_FILE" ] && [ -s "$DATA_FILE" ]
+then
     echo "Data dump $DATA_FILE already exists, skipping data generation"
 else
-    echo "Dumping data from $DB_NAME@$DB_HOST as user $DB_USER... (this may take a while)"
+    echo "Dumping db from $DB_NAME@$DB_HOST as user $DB_USER... (this may take a while)"
 
     PGPASSWORD=$PASSWORD pg_dump \
         -h $DB_HOST \
@@ -17,36 +33,24 @@ else
         --no-privileges \
         --no-acl \
         --format custom \
-        --file $DATA_FILE \
-        --column-inserts \
-        --data-only \
-        --table=traveller \
-        --table=operatorcode \
-        --table=operator \
-        --table=brand \
-        --table=stock \
-        --table=stocksubclass \
-        --table=stockformation \
-        --table=operatorstock \
-        --table=station \
-        --table=stationname \
-        --table=stationpoint \
-        --table=service \
-        --table=serviceendpoint \
-        --table=call \
-        --table=associatedservice \
-        --table=leg \
-        --table=legcall \
-        --table=stocksegment \
-        --table=stockreport \
-        --table=stocksegmentreport \
-        --table=busstop \
-        --table=busoperator \
-        --table=busservice \
-        --table=busservicevia \
-        --table=busjourney \
-        --table=buscall \
-        --table=busmodel \
-        --table=busvehicle \
-        --table=busleg
+        --file $DATA_FILE
 fi
+
+DB_DATA_DIR="/db/data"
+
+for DATA_FILE in `find $DB_DATA_DIR -name *sql`
+do
+    cp $DATA_FILE "$DB_POPULATE_DIR/1_data_${DATA_FILE#$DB_DATA_DIR/}"
+done
+
+DB_CODE_DIR="/db/code"
+
+for CODE_FILE in `find $DB_CODE_DIR -name *sql`
+do
+    FILE_NAME="2_code_${CODE_FILE#$DB_CODE_DIR/}"
+    echo "file name is $FILE_NAME"
+    OUTPUT_FILE=$( echo $FILE_NAME | sed 's/\//_/g' )
+    echo "output is $OUTPUT_FILE"
+    cp $CODE_FILE "$DB_POPULATE_DIR/$OUTPUT_FILE"
+done
+
