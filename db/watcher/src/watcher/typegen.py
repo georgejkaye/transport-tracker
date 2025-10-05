045bf5069938274462057fdb951500112966ef52
George Kaye <georgejkaye@gmail.com>
2025-10-05 17:04:57 +0100

    feat: add file for generating type files

---
+++ db/watcher/src/watcher/typegen.py
@@ -0,0 +1,142 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+tab = "    "
+
+
+@dataclass
+class PostgresTypeField:
+    field_name: str
+    field_type: str
+
+
+@dataclass
+class PostgresType:
+    type_name: str
+    type_fields: list[PostgresTypeField]
+
+
+postgres_to_python_type_dict = {
+    "TEXT": "str",
+    "INT": "int",
+    "INTEGER": "int",
+    "DECIMAL": "Decimal",
+    "TIMESTAMP WITH TIME ZONE": "datetime",
+    "TIMESTAMP WITHOUT TIME ZONE": "datetime",
+    "INTERVAL": "timedelta",
+    "DATERANGE": "Range",
+}
+
+
+def get_python_type_name_for_postgres_type_name(type_name: str) -> str:
+    return "".join(x.capitalize() for x in type_name.lower().split("_"))
+
+
+def get_python_type_for_base_postgres_type(base_type_string: str) -> str:
+    if (
+        base_python_type := postgres_to_python_type_dict.get(base_type_string)
+    ) is not None:
+        return base_python_type
+    return get_python_type_name_for_postgres_type_name(base_type_string)
+
+
+def get_python_type_for_postgres_type(type_string: str) -> str:
+    is_array_type = type_string[-2:] == "[]"
+    if is_array_type:
+        base_type_string = type_string[:-2]
+    else:
+        base_type_string = type_string
+    base_python_type = get_python_type_for_base_postgres_type(base_type_string)
+    if is_array_type:
+        return f"list[{base_python_type}]"
+    return base_python_type
+
+
+def get_python_for_postgres_type(postgres_type: PostgresType) -> str:
+    python_type_name = get_python_type_name_for_postgres_type_name(
+        postgres_type.type_name
+    )
+    python_type_declaration = f"class {python_type_name}:"
+    python_lines = ["@dataclass", python_type_declaration]
+    for type_field in postgres_type.type_fields:
+        python_type = get_python_type_for_postgres_type(type_field.field_type)
+        python_type_field_str = f"{tab}{type_field.field_name}: {python_type}"
+        python_lines.append(python_type_field_str)
+    return "\n".join(python_lines)
+
+
+def get_postgres_type_for_postgres_type_string(
+    postgres_type_str: str,
+) -> PostgresType:
+    one_line_string = " ".join(
+        postgres_type_str.replace("\n", "")
+        .replace(")", "")
+        .replace(";", "")
+        .split()
+    )
+    string_split_at_bracket = one_line_string.split("(", 1)
+    postgres_create_type_clause = string_split_at_bracket[0]
+    postgres_type_name = postgres_create_type_clause.split(" ")[2]
+    postgres_type_field_clause = string_split_at_bracket[1]
+    postgres_type_fields: list[PostgresTypeField] = []
+    for type_clause in postgres_type_field_clause.split(","):
+        type_clause_clauses = type_clause.strip().split(" ")
+        postgres_type_field_name = type_clause_clauses[0]
+        postgres_type_field_type = type_clause_clauses[1]
+        postgres_type_field = PostgresTypeField(
+            postgres_type_field_name, postgres_type_field_type
+        )
+        postgres_type_fields.append(postgres_type_field)
+    return PostgresType(postgres_type_name, postgres_type_fields)
+
+
+def get_postgres_types_for_postgres_type_file_str(
+    postgres_type_file_str: str,
+) -> list[PostgresType]:
+    postgres_statement_strs = (
+        postgres_type_file_str.replace("\n", "").strip().split(";")
+    )
+    postgres_types = [
+        get_postgres_type_for_postgres_type_string(postgres_statement_str)
+        for postgres_statement_str in postgres_statement_strs[:-1]
+    ]
+    return postgres_types
+
+
+def get_imports_for_python_code_str(python_code_str: str) -> list[str]:
+    python_imports: list[str] = ["from dataclasses import dataclass"]
+    if ": datetime" in python_code_str:
+        python_imports.append("from datetime import datetime")
+    if ": timedelta" in python_code_str:
+        python_imports.append("from datetime import timedelta")
+    if ": Decimal" in python_code_str:
+        python_imports.append("from decimal import Decimal")
+    if ": Range" in python_code_str:
+        python_imports.append("from psycopg.types.range import Range")
+    return python_imports
+
+
+def get_python_code_for_postgres_types(
+    postgres_types: list[PostgresType],
+) -> str:
+    python_type_codes = [
+        get_python_for_postgres_type(postgres_type)
+        for postgres_type in postgres_types
+    ]
+    python_code_str = "\n\n\n".join(python_type_codes)
+    python_imports = get_imports_for_python_code_str(python_code_str)
+    python_import_str = "\n".join(python_imports)
+    return f"{python_import_str}\n\n{python_code_str}"
+
+
+def get_postgres_types_for_postgres_type_file(
+    file_path: str | Path,
+) -> list[PostgresType]:
+    with open(file_path, "r") as f:
+        file_contents = f.read()
+    return get_postgres_types_for_postgres_type_file_str(file_contents)
+
+
+def get_python_code_for_postgres_type_file(file_path: str | Path) -> str:
+    postgres_types = get_postgres_types_for_postgres_type_file(file_path)
+    return get_python_code_for_postgres_types(postgres_types)
