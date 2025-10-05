9ba972733dde9b8bd00c1914ff5e34cc8d4633fb
George Kaye <georgejkaye@gmail.com>
2025-10-05 23:45:28 +0100

    feat: add internal watcher script running in

--- db/watcher/src/watcher/main.py
+++ db/watcher/src/watcher/main.py
@@ -54,38 +54,42 @@ def run_in_script_file(script_file: str):
     print()
 
 
-def run_in_scripts(code_dir: str):
+def run_in_scripts(internal_scripts_path: str, user_scripts_path: str):
     global last_trigger_time
     current_time = datetime.now()
     if (current_time - last_trigger_time) > timedelta(seconds=1):
-        files = get_db_scripts_files(code_dir)
-        for file in files:
+        internal_files = get_db_scripts_files(internal_scripts_path)
+        for file in internal_files:
+            run_in_script_file(file)
+        user_files = get_db_scripts_files(user_scripts_path)
+        for file in user_files:
             run_in_script_file(file)
         last_trigger_time = datetime.now()
 
 
 class MyEventHandler(FileSystemEventHandler):
-    def __init__(self, path: str):
-        self.path = path
+    def __init__(self, internal_scripts_path: str, user_scripts_path: str):
+        self.internal_scripts_path = internal_scripts_path
+        self.user_scripts_path = user_scripts_path
 
     def on_created(self, event: FileSystemEvent):
-        run_in_scripts(self.path)
+        run_in_scripts(self.internal_scripts_path, self.user_scripts_path)
 
     def on_modified(self, event: FileSystemEvent):
-        run_in_scripts(self.path)
+        run_in_scripts(self.internal_scripts_path, self.user_scripts_path)
 
     def on_moved(self, event: FileSystemEvent):
-        run_in_scripts(self.path)
+        run_in_scripts(self.internal_scripts_path, self.user_scripts_path)
 
 
-def main(path: str):
-    event_handler = MyEventHandler(path)
+def main(internal_scripts_path: str, user_scripts_path: str):
+    event_handler = MyEventHandler(internal_scripts_path, user_scripts_path)
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, user_scripts_path, recursive=True)
 
     # Start the observe
     observer.start()
-    print(f"Watching script files in: {path}", flush=True)
+    print(f"Watching script files in: {user_scripts_path}", flush=True)
 
     try:
         while True:
@@ -96,4 +100,4 @@ def main(path: str):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
