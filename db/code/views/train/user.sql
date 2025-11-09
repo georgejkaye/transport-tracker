4d8ed14c917889bbb18cb0c1c5371006fc71ffea
George Kaye <georgejkaye@gmail.com>
2025-11-09 15:12:07 +0000

    feat: rewrite station leg counts view

--- db/code/views/train/user.sql
+++ db/code/views/train/user.sql
@@ -1,5 +1,5 @@
 DROP VIEW IF EXISTS transport_user_train_leg_view;
--- DROP VIEW IF EXISTS transport_user_train_station_view;
+DROP VIEW IF EXISTS transport_user_train_station_view;
 
 CREATE VIEW transport_user_train_leg_view AS
 SELECT
@@ -96,101 +96,89 @@ ON train_service.train_operator_id = train_operator.train_operator_id
 LEFT JOIN train_brand
 ON train_service.train_brand_id = train_brand.train_brand_id;
 
--- CREATE OR REPLACE VIEW transport_user_train_station_view AS
--- WITH
---     train_leg_time AS (
---         SELECT
---             train_leg_id,
---             MIN(COALESCE(plan_arr, plan_dep, act_arr, act_dep)) AS board_time,
---             MAX(COALESCE(plan_arr, plan_dep, act_arr, act_dep)) AS alight_time
---         FROM train_leg_call
---         INNER JOIN train_call
---         ON train_leg_call.arr_call_id = train_call.train_call_id
---         OR train_leg_call.dep_call_id = train_call.train_call_id
---         GROUP BY train_leg_id
---     ),
---     train_station_leg_call AS (
---         SELECT
---             train_call.train_station_id,
---             train_leg_call.train_leg_id,
---             train_call.train_call_id,
---             COALESCE(plan_arr, plan_dep, act_arr, act_dep) AS call_time
---         FROM train_leg_call
---         INNER JOIN train_call
---         ON train_leg_call.arr_call_id = train_call.train_call_id
---         OR train_leg_call.dep_call_id = train_call.train_call_id
---     )
--- SELECT
---     transport_user_train_leg.user_id,
---     train_station.train_station_id,
---     train_station.station_crs,
---     train_station.station_name,
---     (
---         train_operator.train_operator_id,
---         train_operator.operator_code,
---         train_operator.operator_name
---     )::transport_user_train_station_operator_out_data,
---     train_station_number.boards,
---     train_station_number.alights,
---     train_station_number.boards,
---     CASE WHEN train_brand.train_brand_id IS NULL
---     THEN
---         NULL
---     ELSE
---         (
---             train_operator.train_operator_id,
---             train_operator.operator_code,
---             train_operator.operator_name
---         )::transport_user_train_station_operator_out_data,
---     train_station_leg.station_legs
--- FROM transport_user_train_leg
--- INNER JOIN (
---     SELECT user_id, train_station_id, COUNT(*)
---     FROM transport_user_train_leg
---     INNER JOIN train_leg_call
---     ON transport_user_train_leg.train_leg_id = train_leg_call.train_leg_id
---     INNER JOIN train_call
---     ON train_leg_call.arr_call_id = train_call.train_call_id
---     OR train_leg_call.dep_call_id = train_call.train_call_id
---     GROUP BY user_id, train_station_id
--- ) transport_user_train_station
--- ON transport_user_train_leg.user_id = transport_user_train_station.user_id
--- INNER JOIN (
---     SELECT user_id, train_station_id, COUNT(*)
---     FROM transport_user_train_leg
---     INNER JOIN train_leg_call
---     ON transport_user_train_leg.train_leg_id = train_leg_call.train_leg_id
---     INNER JOIN train_call
---     ON train_leg_call.arr_call_id = train_call.train_call_id
---     OR train_leg_call.dep_call_id = train_call.train_call_id
---     INNER JOIN train_leg_time
-
---     GROUP BY user_id, train_station_id
--- )
-
--- FROM train_station
--- INNER JOIN train_operator
--- ON train_station.train_operator_id = train_operator.train_operator_id
--- LEFT JOIN train_brand
--- ON train_station.train_brand_id = train_brand.train_brand_id
--- INNER JOIN
-
--- SELECT
---     train_leg_call.train_leg_id,
---     train_call_alight.train_call_id,
---     train_call_alight.train_station_id,
---     train_call_board.train_call_id,
---     train_call_board.train_station_id
---     FROM (
---         SELECT
---             train_leg_id,
---             MIN(COALESCE(plan_arr, plan_dep, act_arr, act_dep)) AS board_time,
---             MAX(COALESCE(plan_arr, plan_dep, act_arr, act_dep)) AS alight_time
---         FROM train_leg_call
---         INNER JOIN train_call
---         ON train_leg_call.arr_call_id = train_call.train_call_id
---         OR train_leg_call.dep_call_id = train_call.train_call_id
---         GROUP BY train_leg_id) train_leg_time O
---     FROM train_call train_call_alight
-
---     train_station.station_name FROM train_call INNER JOIN N COALESCE(plan_arr, plan_dep, act_arr, act_dep) = train_leg_time.board_time INNER JOIN train_station ON train_call.train_station_id = train_station.train_station_id WHERE train_leg_id = 1001;
+CREATE OR REPLACE VIEW transport_user_train_station_view AS
+WITH
+    train_leg_board_alight_time_view AS (
+        SELECT
+            train_leg_id,
+            MIN(COALESCE(plan_arr, plan_dep, act_arr, act_dep)) AS board_time,
+            MAX(COALESCE(plan_arr, plan_dep, act_arr, act_dep)) AS alight_time
+        FROM train_leg_call
+        INNER JOIN train_call
+        ON train_leg_call.arr_call_id = train_call.train_call_id
+        OR train_leg_call.dep_call_id = train_call.train_call_id
+        GROUP BY train_leg_id
+    ),
+    train_station_leg_call_time_view AS (
+        SELECT
+            train_call.train_station_id,
+            train_leg_call.train_leg_id,
+            train_call.train_call_id,
+            COALESCE(plan_arr, plan_dep, act_arr, act_dep) AS call_time
+        FROM train_leg_call
+        INNER JOIN train_call
+        ON train_leg_call.arr_call_id = train_call.train_call_id
+        OR train_leg_call.dep_call_id = train_call.train_call_id
+    )
+SELECT
+    transport_user_train_station_leg_count_view.user_id,
+    train_station.train_station_id,
+    train_station.station_crs,
+    train_station.station_name,
+    (
+        train_operator.train_operator_id,
+        train_operator.operator_code,
+        train_operator.operator_name
+    )::transport_user_train_station_operator_out_data AS station_operator,
+    CASE WHEN train_brand.train_brand_id IS NULL
+    THEN
+        NULL
+    ELSE
+        (
+            train_brand.train_brand_id,
+            train_brand.brand_code,
+            train_brand.brand_name
+        )::transport_user_train_station_operator_out_data
+    END AS station_brand,
+    transport_user_train_station_leg_count_view.boards,
+    transport_user_train_station_leg_count_view.alights,
+    transport_user_train_station_leg_count_view.calls
+    -- train_station_leg.station_legs
+FROM train_station
+INNER JOIN train_operator
+ON train_station.train_operator_id = train_operator.train_operator_id
+LEFT JOIN train_brand
+ON train_station.train_brand_id = train_brand.train_brand_id
+INNER JOIN (
+    SELECT
+        transport_user_train_leg.user_id,
+        train_station_id,
+        COUNT(
+            CASE WHEN
+                call_time = board_time
+            THEN 1
+            END
+        ) AS boards,
+        COUNT(
+            CASE WHEN
+                call_time = alight_time
+            THEN 1
+            END
+        ) AS alights,
+        COUNT(
+            CASE WHEN
+                call_time NOT IN (board_time, alight_time)
+            THEN 1
+            END
+        ) AS calls
+    FROM transport_user_train_leg
+    INNER JOIN train_station_leg_call_time_view
+    ON transport_user_train_leg.train_leg_id
+        = train_station_leg_call_time_view.train_leg_id
+    INNER JOIN train_leg_board_alight_time_view
+    ON train_station_leg_call_time_view.train_leg_id
+        = train_leg_board_alight_time_view.train_leg_id
+    GROUP BY user_id, train_station_id
+) transport_user_train_station_leg_count_view
+ON train_station.train_station_id
+    = transport_user_train_station_leg_count_view.train_station_id;
