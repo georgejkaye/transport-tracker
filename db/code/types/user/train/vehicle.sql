ba049502684543347d98f53c7e9af78a8842c9f9
George Kaye <georgejkaye@gmail.com>
2025-12-08 22:13:17 +0000

    feat: add more fields to class output data

--- db/code/types/user/train/vehicle.sql
+++ db/code/types/user/train/vehicle.sql
@@ -5,11 +5,17 @@ DROP DOMAIN IF EXISTS transport_user_train_class_out_data_notnull CASCADE;
 
 CREATE TYPE transport_user_train_class_leg_out_data AS (
     leg_id INTEGER_NOTNULL,
-    board_station transport_user_train_station_leg_endpoint_out_data_notnull,
-    alight_station transport_user_train_station_leg_endpoint_out_data_notnull,
-    unit_no INTEGER_NOTNULL,
-    distance DECIMAL_NOTNULL,
-    duration INTERVAL_NOTNULL
+    unit_number INTEGER,
+    stock_subclass INTEGER,
+    stock_cars INTEGER,
+    stock_start_station train_leg_station_out_data_notnull,
+    stock_end_station train_leg_station_out_data_notnull,
+    distance DECIMAL,
+    duration INTERVAL,
+    leg_board_station train_leg_station_out_data_notnull,
+    leg_alight_station train_leg_station_out_data_notnull,
+    operator train_leg_operator_out_data,
+    brand train_leg_operator_out_data
 );
 
 CREATE DOMAIN transport_user_train_class_leg_out_data_notnull
