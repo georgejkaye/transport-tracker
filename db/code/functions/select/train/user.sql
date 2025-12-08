2a176e74f4cb243438e9eed7e3dec1443b26a451
George Kaye <georgejkaye@gmail.com>
2025-12-08 22:14:00 +0000

    feat: add function for getting user class

--- db/code/functions/select/train/user.sql
+++ db/code/functions/select/train/user.sql
@@ -72,3 +72,20 @@ WHERE user_id = p_user_id
 AND train_station_id = p_train_station_id
 ORDER BY station_name ASC;
 $$;
+
+CREATE FUNCTION select_transport_user_train_stock_class_by_user_id (
+    p_user_id INTEGER_NOTNULL,
+    p_stock_class INTEGER_NOTNULL
+)
+RETURNS SETOF transport_user_train_class_out_data
+LANGUAGE sql
+AS
+$$
+SELECT
+    transport_user_train_stock_class_view.stock_class,
+    transport_user_train_stock_class_view.name,
+    transport_user_train_stock_class_view.class_legs
+FROM transport_user_train_stock_class_view
+WHERE user_id = p_user_id
+AND stock_class = p_stock_class;
+$$;
