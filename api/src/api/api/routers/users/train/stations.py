d860f7786c0c327c613b68e24d3aae3e966f1e63
George Kaye <georgejkaye@gmail.com>
2025-12-29 22:32:11 +0000

    feat: add new user station endpoints

--- api/src/api/api/routers/users/train/stations.py
+++ api/src/api/api/routers/users/train/stations.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import APIRouter, HTTPException
 
 from api.api.lifespan import get_db_connection
@@ -6,17 +8,35 @@ from api.db.functions.select.train.user.station import (
     select_transport_user_train_station_by_user_id_and_station_id_fetchone,
     select_transport_user_train_stations_by_user_id_fetchall,
 )
-from api.db.types.user.train.station import TransportUserTrainStationOutData
+from api.db.types.user.train.station import (
+    TransportUserTrainStationHighOutData,
+    TransportUserTrainStationOutData,
+)
 
 router = APIRouter(prefix="/stations", tags=["users/train/stations"])
 
 
 @router.get("", summary="Get train station data for a user")
 async def get_train_stations(
-    user_id: int,
-) -> list[TransportUserTrainStationOutData]:
+    user_id: int, search_start: datetime, search_end: datetime
+) -> list[TransportUserTrainStationHighOutData]:
     stations = select_transport_user_train_stations_by_user_id_fetchall(
-        get_db_connection(), user_id
+        get_db_connection(), user_id, search_start, search_end
+    )
+    return stations
+
+
+@router.get(
+    "/years/{year}", summary="Get train station data for a user in a given year"
+)
+async def get_train_stations_by_year(
+    user_id: int, year: int
+) -> list[TransportUserTrainStationHighOutData]:
+    stations = select_transport_user_train_stations_by_user_id_fetchall(
+        get_db_connection(),
+        user_id,
+        datetime(year, 1, 1),
+        datetime(year + 1, 1, 1),
     )
     return stations
 
@@ -26,11 +46,35 @@ async def get_train_stations(
     summary="Get train station data for a user and a particular station",
 )
 async def get_train_station(
-    user_id: int, station_id: int
+    user_id: int, station_id: int, search_start: datetime, search_end: datetime
 ) -> TransportUserTrainStationOutData:
     station = (
         select_transport_user_train_station_by_user_id_and_station_id_fetchone(
-            get_db_connection(), user_id, station_id
+            get_db_connection(), user_id, station_id, search_start, search_end
+        )
+    )
+    if station is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Train station id {station_id} not found",
+        )
+    return station
+
+
+@router.get(
+    "/{station_id}/years/{year}",
+    summary="Get train station data for a user and a particular station",
+)
+async def get_train_station_by_year(
+    user_id: int, station_id: int, year: int
+) -> TransportUserTrainStationOutData:
+    station = (
+        select_transport_user_train_station_by_user_id_and_station_id_fetchone(
+            get_db_connection(),
+            user_id,
+            station_id,
+            datetime(year, 1, 1),
+            datetime(year + 1, 1, 1),
         )
     )
     if station is None:
@@ -43,11 +87,32 @@ async def get_train_station(
 
 @router.get("/crs/{station_crs}", summary="Get train station")
 async def get_train_station_by_crs(
-    user_id: int, station_crs: str
+    user_id: int, station_crs: str, search_start: datetime, search_end: datetime
 ) -> TransportUserTrainStationOutData:
     station = (
         select_transport_user_train_station_by_user_id_and_station_crs_fetchone(
-            get_db_connection(), user_id, station_crs
+            get_db_connection(), user_id, station_crs, search_start, search_end
+        )
+    )
+    if station is None:
+        raise HTTPException(status_code=404, detail="Station not found")
+    return station
+
+
+@router.get(
+    "/crs/{station_crs}/years/{year}",
+    summary="Get train station data for a user in a given year",
+)
+async def get_train_station_by_crs_and_year(
+    user_id: int, station_crs: str, year: int
+) -> TransportUserTrainStationOutData:
+    station = (
+        select_transport_user_train_station_by_user_id_and_station_crs_fetchone(
+            get_db_connection(),
+            user_id,
+            station_crs,
+            datetime(year, 1, 1),
+            datetime(year + 1, 1, 1),
         )
     )
     if station is None:
