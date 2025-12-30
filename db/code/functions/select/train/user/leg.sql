cae031bd9d45f055354c9d5f2c2c12e23f85d642
George Kaye <georgejkaye@gmail.com>
2025-12-30 22:19:27 +0000

    feat: begin adding stats retrieval

--- db/code/functions/select/train/user/leg.sql
+++ db/code/functions/select/train/user/leg.sql
@@ -1,5 +1,31 @@
 DROP FUNCTION IF EXISTS select_transport_user_train_legs_by_user_id;
 
+CREATE FUNCTION select_transport_user_train_leg_stats_by_user_id (
+    p_user_id INTEGER_NOTNULL,
+    p_search_start TIMESTAMP WITH TIME ZONE,
+    p_search_end TIMESTAMP WITH TIME ZONE
+)
+RETURNS SETOF transport_user_train_leg_stats
+LANGUAGE sql
+AS
+$$
+SELECT
+    COUNT(*) AS count,
+    SUM(distance) AS total_distance,
+    MAX(distance) AS longest_distance,
+    MIN(distance) AS shortest_distance,
+    SUM(duration) AS total_duration,
+    MAX(duration) AS longest_duration,
+    MIN(duration) AS shortest_duration,
+    SUM(delay) AS total_delay,
+    MAX(delay) AS longest_delay,
+    MIN(delay) AS shortest_delay
+FROM transport_user_train_leg_view
+WHERE user_id = p_user_id
+AND (p_search_start IS NULL OR start_datetime >= p_search_start)
+AND (p_search_end IS NULL OR start_datetime < p_search_end)
+$$;
+
 CREATE FUNCTION select_transport_user_train_legs_by_user_id (
     p_user_id INTEGER_NOTNULL,
     p_search_start TIMESTAMP WITH TIME ZONE,
@@ -24,4 +50,60 @@ WHERE user_id = p_user_id
 AND (p_search_start IS NULL OR start_datetime >= p_search_start)
 AND (p_search_end IS NULL OR start_datetime < p_search_end)
 ORDER BY start_datetime ASC;
+$$;
+
+CREATE FUNCTION select_longest_transport_user_train_legs_by_user_id (
+    p_user_id INTEGER_NOTNULL,
+    p_search_start TIMESTAMP WITH TIME ZONE,
+    p_search_end TIMESTAMP WITH TIME ZONE,
+    p_rows_to_return INTEGER
+)
+RETURNS SETOF transport_user_train_leg_out_data
+LANGUAGE sql
+AS
+$$
+SELECT
+    train_leg_id,
+    board_station,
+    alight_station,
+    start_datetime,
+    operator,
+    brand,
+    distance,
+    duration,
+    delay
+FROM transport_user_train_leg_view
+WHERE user_id = p_user_id
+AND (p_search_start IS NULL OR start_datetime >= p_search_start)
+AND (p_search_end IS NULL OR start_datetime < p_search_end)
+ORDER BY distance DESC
+LIMIT p_rows_to_return;
+$$;
+
+CREATE FUNCTION select_shortest_transport_user_train_legs_by_user_id (
+    p_user_id INTEGER_NOTNULL,
+    p_search_start TIMESTAMP WITH TIME ZONE,
+    p_search_end TIMESTAMP WITH TIME ZONE,
+    p_rows_to_return INTEGER
+)
+RETURNS SETOF transport_user_train_leg_out_data
+LANGUAGE sql
+AS
+$$
+SELECT
+    train_leg_id,
+    board_station,
+    alight_station,
+    start_datetime,
+    operator,
+    brand,
+    distance,
+    duration,
+    delay
+FROM transport_user_train_leg_view
+WHERE user_id = p_user_id
+AND (p_search_start IS NULL OR start_datetime >= p_search_start)
+AND (p_search_end IS NULL OR start_datetime < p_search_end)
+ORDER BY distance ASC
+LIMIT p_rows_to_return;
 $$;
--- db/code/types/user/train/leg.sql
+++ db/code/types/user/train/leg.sql
@@ -1,10 +1,11 @@
 DROP TYPE IF EXISTS transport_user_train_leg_out_data CASCADE;
+DROP TYPE IF EXISTS transport_user_train_leg_stats CASCADE;
 
 CREATE TYPE transport_user_train_leg_out_data AS (
-    leg_id INTEGER,
+    leg_id INTEGER_NOTNULL,
     board_station train_station_high_out_data_notnull,
     alight_station train_station_high_out_data_notnull,
-    start_datetime TIMESTAMP WITH TIME ZONE,
+    start_datetime TIMESTAMP_NOTNULL,
     operator train_operator_high_out_data_notnull,
     brand train_operator_high_out_data,
     distance DECIMAL,
@@ -14,3 +15,16 @@ CREATE TYPE transport_user_train_leg_out_data AS (
 
 CREATE DOMAIN transport_user_train_leg_out_data_notnull
 AS transport_user_train_leg_out_data NOT NULL;
+
+CREATE TYPE transport_user_train_leg_stats AS (
+    count INTEGER_NOTNULL,
+    total_distance DECIMAL_NOTNULL,
+    longest_distance DECIMAL_NOTNULL,
+    shortest_distance DECIMAL_NOTNULL,
+    total_duration INTERVAL_NOTNULL,
+    longest_duration INTERVAL_NOTNULL,
+    shortest_duration INTERVAL_NOTNULL,
+    total_delay INTEGER_NOTNULL,
+    longest_delay INTEGER_NOTNULL,
+    shortest_delay INTEGER_NOTNULL
+);
