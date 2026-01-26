5d0ccc5e8a36e823ca162bf54a5c52c8566f714b
George Kaye <georgejkaye@gmail.com>
2026-01-26 21:55:52 +0000

    feat: add top level user stats route

--- api/src/api/api/routers/users/user.py
+++ api/src/api/api/routers/users/user.py
@@ -1,9 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
+from api.api.lifespan import get_db_connection
 from api.api.routers.users.bus import bus
 from api.api.routers.users.train import train
+from api.db.functions.select.user.stats import (
+    select_transport_user_details_fetchone,
+)
+from api.db.types.user.user import TransportUserDetailsOutData
 
 router = APIRouter(prefix="/users/{user_id}")
 
 router.include_router(train.router)
 router.include_router(bus.router)
+
+
+@router.get("", summary="Get transport overview for a user")
+async def get_user_details(user_id: int) -> TransportUserDetailsOutData:
+    details = select_transport_user_details_fetchone(
+        get_db_connection(), user_id
+    )
+    if details is None:
+        raise HTTPException(404, f"Could not get user {user_id}")
+    return details
