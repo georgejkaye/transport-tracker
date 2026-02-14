1c5db92ecfb094d72b1b32a91228046d3be831ec
George Kaye <georgejkaye@gmail.com>
2026-02-14 22:14:55 +0000

    feat: add station and new station graphs

--- client/src/app/users/[userId]/train/years/[year]/StatsGraphs.tsx
+++ client/src/app/users/[userId]/train/years/[year]/StatsGraphs.tsx
@@ -11,12 +11,18 @@ import { getDurationStringFromMinutes } from "@/app/utils/duration"
 
 interface YearTaggedDatum {
   year: number
-  value: number
+  value: number[]
+}
+
+interface YearTaggedSeriesStyle {
+  name?: string
+  colour?: string
 }
 
 interface YearGraphProps {
   title: string
   data: YearTaggedDatum[]
+  style: YearTaggedSeriesStyle[]
   popupFormatter: (value: number) => string
   axisFormatter: (value: number) => string
   yAxisWidth: number
@@ -25,6 +31,7 @@ interface YearGraphProps {
 const YearGraph = ({
   title,
   data,
+  style,
   popupFormatter,
   axisFormatter,
   yAxisWidth,
@@ -33,19 +40,24 @@ const YearGraph = ({
   let yearLabels = sortedData.map((datum) =>
     datum.year.toString().replace(",", ""),
   )
-  let yData = sortedData.map((datum) => datum.value)
+  let seriesCount = sortedData[0]?.value.length ?? 0
+  let yData = Array.from({ length: seriesCount }, (_, i) =>
+    Array.from(
+      { length: sortedData.length },
+      (_, j) => sortedData[j]?.value[i] ?? 0,
+    ),
+  )
   return (
     <div className="flex-1 p-4 shadow-lg" style={{ height: "300px" }}>
       <h2 className="font-bold text-lg">{title}</h2>
       <LineChart
-        series={[
-          {
-            data: yData,
-            id: title,
-            valueFormatter: (x) => (!x ? "" : popupFormatter(x)),
-            color: "#003366",
-          },
-        ]}
+        series={yData.map((series, i) => ({
+          data: series,
+          id: style[i]?.name ?? `${title}-${i}`,
+          valueFormatter: (x) => (!x ? "" : popupFormatter(x)),
+          label: style[i]?.name,
+          color: "#003366",
+        }))}
         xAxis={[{ scaleType: "point", data: yearLabels, height: 28 }]}
         yAxis={[
           {
@@ -54,6 +66,7 @@ const YearGraph = ({
             width: yAxisWidth,
           },
         ]}
+        hideLegend
       />
     </div>
   )
@@ -72,39 +85,58 @@ const StatsGraphs = ({
 }: StatsGraphsProps) => {
   let legCountData = trainLegStats.map((yearData) => ({
     year: yearData.year,
-    value: yearData.count,
+    value: [yearData.count],
   }))
   let legDistanceData = trainLegStats.map((yearData) => ({
     year: yearData.year,
-    value: Number(yearData.total_distance),
+    value: [Number(yearData.total_distance)],
   }))
   let legDurationData = trainLegStats.map((yearData) => ({
     year: yearData.year,
-    value: Duration.parse(yearData.total_duration).toMinutes(),
+    value: [Duration.parse(yearData.total_duration).toMinutes()],
+  }))
+  let stationCountData = trainStationStats.map((yearData) => ({
+    year: yearData.year,
+    value: [yearData.station_count, yearData.new_station_count],
   }))
   return (
-    <div className="flex flex-col md:flex-row">
-      <YearGraph
-        data={legCountData}
-        title="Count"
-        popupFormatter={(x) => `${x}`}
-        axisFormatter={(x) => `${x}`}
-        yAxisWidth={30}
-      />
-      <YearGraph
-        data={legDistanceData}
-        title="Distance"
-        popupFormatter={getMilesAndChainsStringFromNumber}
-        axisFormatter={(x) => `${Math.floor(x)} mi`}
-        yAxisWidth={60}
-      />
-      <YearGraph
-        data={legDurationData}
-        title="Duration"
-        popupFormatter={getDurationStringFromMinutes}
-        axisFormatter={(x) => `${Math.floor(x / 60)} h`}
-        yAxisWidth={40}
-      />
+    <div className="flex flex-col">
+      <div className="flex flex-col md:flex-row">
+        <YearGraph
+          data={legCountData}
+          title="Count"
+          style={[{ name: "Count" }]}
+          popupFormatter={(x) => `${x}`}
+          axisFormatter={(x) => `${x}`}
+          yAxisWidth={30}
+        />
+        <YearGraph
+          data={legDistanceData}
+          title="Distance"
+          style={[{ name: "Distance" }]}
+          popupFormatter={getMilesAndChainsStringFromNumber}
+          axisFormatter={(x) => `${Math.floor(x)} mi`}
+          yAxisWidth={60}
+        />
+        <YearGraph
+          data={legDurationData}
+          title="Duration"
+          style={[{ name: "Duration" }]}
+          popupFormatter={getDurationStringFromMinutes}
+          axisFormatter={(x) => `${Math.floor(x / 60)} h`}
+          yAxisWidth={40}
+        />
+      </div>
+      <div>
+        <YearGraph
+          data={stationCountData}
+          title="Stations"
+          style={[{ name: "Total stations" }, { name: "New stations" }]}
+          popupFormatter={(x) => `${x}`}
+          axisFormatter={(x) => `${x}`}
+          yAxisWidth={30}
+        />
+      </div>
     </div>
   )
 }
